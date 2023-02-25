dcefb53c4bc5fbf74d93a4ec938e219771ead3cd
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2023-02-25 06:41:41 +0530

    MYSQL

---
+++ Day10/ImageIconExam.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class ImageIconExam {
+  ImageIconExam() {
+    JFrame fr = new JFrame("Iamge Icon ");
+    fr.setSize(30, 30);
+    fr.setVisible(true);
+    JPanel jp = new JPanel();
+    fr.add(jp);
+    ImageIcon img = new ImageIcon("download.jpg");
+    JButton jb = new JButton(img);
+    jp.add(jb);
+    JLabel jl = new JLabel(img);
+    jp.add(jl);
+
+  }
+
+  public static void main(String arg[]) {
+    ImageIconExam obj = new ImageIconExam();
+  }
+}
